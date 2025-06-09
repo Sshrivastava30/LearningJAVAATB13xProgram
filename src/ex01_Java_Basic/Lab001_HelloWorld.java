33aab286de1e52a040a7ee0b33d47903bc4ad99f
Sshrivastava30 <srivastavashivangi30@gmail.com>
2025-06-10 00:25:33 +0530

    initial hello commit

---
+++ src/ex01_Java_Basic/Lab001_HelloWorld.java
@@ -0,0 +1,8 @@
+package ex01_Java_Basic;
+
+public class Lab001_HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I am running a Java Program");
+    }
+}
---
+++ src/ex01_Java_Basic/Lab002_HW_Extra.java
@@ -0,0 +1,7 @@
+package ex01_Java_Basic;
+
+public class Lab002_HW_Extra {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+    }
+}
